1dee10c2a0c71006e739431445662dde22900a00
krdav <krdav@cbs.dtu.dk>
2018-01-22 14:44:47 -0800

    accomodate isotype data

--- bin/validaggreg_compare.py
+++ bin/validaggreg_compare.py
@@ -44,24 +44,25 @@ p = sns.factorplot(x="method", y="value", col="metric", col_wrap=2,
 p.set_xticklabels(rotation=30)
 plt.savefig(args.outbase+'_violin.pdf')
 
-plt.figure()
-RF_cat = list(set(aggdat['RF']))
-RFs = list()
-correlations = list()
-for i in RF_cat:
-    sl = aggdat[aggdat['RF'] == i]
-    if len(aggdat[aggdat['RF'] == i]) < 10:
-        continue
-    corr_tup = pearsonr(sl['MRCA'], sl['COAR'])
-    if corr_tup[0]:
-        correlations.append(corr_tup[0])
-        RFs.append(str(i))
+if 'RF' in aggdat:
+    plt.figure()
+    RF_cat = list(set(aggdat['RF']))
+    RFs = list()
+    correlations = list()
+    for i in RF_cat:
+        sl = aggdat[aggdat['RF'] == i]
+        if len(aggdat[aggdat['RF'] == i]) < 10:
+            continue
+        corr_tup = pearsonr(sl['MRCA'], sl['COAR'])
+        if corr_tup[0]:
+            correlations.append(corr_tup[0])
+            RFs.append(str(i))
 
-if len(correlations) > 0:
-    df = pd.DataFrame({'correlation':correlations, 'RF':RFs})
-    sns.barplot(x="RF", y="correlation", data=df)
-    plt.savefig(args.outbase+'_MRSAvsCOAR.pdf')
+    if len(correlations) > 0:
+        df = pd.DataFrame({'correlation':correlations, 'RF':RFs})
+        sns.barplot(x="RF", y="correlation", data=df)
+        plt.savefig(args.outbase+'_MRSAvsCOAR.pdf')
 
 
-sns.pairplot(aggdat, kind="reg")
-plt.savefig(args.outbase+'_pairplot.pdf')
+    sns.pairplot(aggdat, kind="reg")
+    plt.savefig(args.outbase+'_pairplot.pdf')
