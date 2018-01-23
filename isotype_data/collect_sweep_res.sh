cce8d9d2fa6cc3667bcd2f90b1c72521df2a6ae1
krdav <krdav@cbs.dtu.dk>
2018-01-22 17:14:03 -0800

    cleaning

--- isotype_data/collect_sweep_res.sh
+++
@@ -1,8 +0,0 @@
-
-# Grep out the clonal families that have misplaced isotypes in either "quick dnapars", JC, HKY or GTR:
-grep -Pv '^[\w\-]+\s+\d+\s+0\s+0\.0$' iso_sweep/*/isotype_validation.tsv | grep -v 'method' | cut -d '/' -f 2 | sort -un > sweep_res.txt
-
-# Get the set of clonal families that will run in dnapars in a reasonable (24h or less) time:
-lt iso_sweep_dnapars/*/heavy/dnapars/dnapars_inferred_tree.tree | cut -d '/' -f 2 > finalset.txt
-
-
