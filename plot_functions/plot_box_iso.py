7823f516167f686233aa998bde12176921212fa9
krdav <krdav@cbs.dtu.dk>
2018-03-05 19:15:28 -0800

    plot difference

--- plot_functions/plot_box_iso.py
+++ plot_functions/plot_box_iso.py
@@ -70,27 +70,39 @@ with PdfPages(args.outbase+'_new.pdf') as pdf_pages:
 
         y = list(range(len(order_m)))
         plt.errorbar(x, y, xerr=errors, fmt = '^', color = '#B22222', barsabove=True, zorder=10)  # set zorder high to overwrite boxplot whiskers
-
         plt.tight_layout()
         pdf_pages.savefig()
 
 
-# Old vertical boxplots:
-'''
-print('Starting to plot')
-plt.figure()
+        # Make boxes of the differences to the best:
+        plt.figure(figsize=(6, 3))
 
-with PdfPages(args.outbase+'_box.pdf') as pdf_pages:
-    p = sns.factorplot(x="method", y="value", col="metric", col_wrap=2,
-                   data=df1, kind="box", size=6, aspect=.65, sharey=False, showfliers=False, showmeans=True)
-    p.set_xticklabels(rotation=30)
-    pdf_pages.savefig(p.fig)
+        diffs = list()
+        errors = [[], []]
+        x = list()
+        for method in order_m[1:]:
+            df_slice = m_df[m_df['method'] == method]['value'].values - m_df[m_df['method'] == order_m[0]]['value'].values
+            diffs.append(df_slice.copy())
+            Nsamples = len(df_slice)
+            boots = np.array([np.mean(np.random.choice(df_slice, Nsamples, replace=True)) for i in range(args.Nboot)])
+            mid = np.percentile(boots, 50)
+            errors[0].append(abs(mid - np.percentile(boots, 2.5)))
+            errors[1].append(abs(mid - np.percentile(boots, 97.5)))
+            x.append(mid)
 
-    p = sns.factorplot(x="method", y="value", col="metric", col_wrap=2,
-                   data=df1, kind="box", size=6, aspect=.65, sharey=False, showfliers=True, showmeans=True)
-    p.set_xticklabels(rotation=30)
-    pdf_pages.savefig(p.fig)
-'''
+        diff_df = pd.DataFrame(list(zip(*diffs)), columns=order_m[1:])
+        diff_df = pd.melt(diff_df, value_vars=order_m[1:], var_name='method')
+
+        p = sns.boxplot(y="method", x="value", data=diff_df, showfliers=o, orient="h", order=order_m[1:], palette=palette_methods)
+        p.axes.set_title('Metric = {}, diff to {}'.format(m, order_m[0]), fontsize=12)
+        p.set_ylabel('')
+        p.set_xlabel('{} difference'.format(m))
+
+        y = list(range(len(order_m[1:])))
+        plt.errorbar(x, y, xerr=errors, fmt = '^', color = '#B22222', barsabove=True, zorder=10)  # set zorder high to overwrite boxplot whiskers
+
+        plt.tight_layout()
+        pdf_pages.savefig()
 
 
 
