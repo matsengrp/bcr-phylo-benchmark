bbe3c1a504f8cdf208bca68e6475fe846089d960
Kristian Davidsen <krdav@cbs.dtu.dk>
2018-04-23 13:00:49 -0700

    only one lambda

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 # This is a minimal test to verify that all tools are setup the right way:
-scons --simulate --igphyml --gctree --samm_rank --dnapars --dnaml --iqtree --iqtree_option_str="-m 000000" --iqtree_option_str="-m 010010" --iqtree_option_str="-m 123450" --outdir=neutral_sim_validation --xvfb --lambda=1.5 --lambda0=0.1825 --lambda0=0.365 --lambda0=0.73 --N=40 --nsim=2 --jobs=1 --random_naive="sequence_data/AbPair_naive_seqs.fa"
+scons --simulate --igphyml --gctree --samm_rank --dnapars --dnaml --iqtree --iqtree_option_str="-m 000000" --iqtree_option_str="-m 010010" --iqtree_option_str="-m 123450" --outdir=neutral_sim_validation --xvfb --lambda=1.5 --lambda0=0.1825 --N=40 --nsim=2 --jobs=1 --random_naive="sequence_data/AbPair_naive_seqs.fa"
